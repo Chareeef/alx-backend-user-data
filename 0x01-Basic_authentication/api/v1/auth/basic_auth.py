88902fcb3a4c8e4f42a1087e91149f3147afa33c
Chareeef <youssef.charif.h@gmail.com>
2024-04-15 18:28:22 +0100

    Decode Base64 authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """The BasicAuth class"""
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -12,8 +14,21 @@ class BasicAuth(Auth):
         """Returns the Base64 part of the Authorization header
         for a Basic Authentication
         """
-        if type(authorization_header) is not str or \
+        if not isinstance(authorization_header, str) or \
                 not authorization_header.startswith('Basic '):
             return None
         else:
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_header: str) -> str:
+        """Returns the decoded value of a
+        Base64 string `base64_header`
+        """
+        if not isinstance(base64_header, str):
+            return None
+
+        try:
+            header_bytes = base64_header.encode('utf-8')
+            return base64.decodebytes(header_bytes).decode('utf-8')
+        except binascii.Error:
+            return None
