cbf0d5a2dad0200d6dce456de37bf1d25a941c80
Chareeef <youssef.charif.h@gmail.com>
2024-04-15 14:13:44 +0100

    Implement Auth.require_auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,21 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        return False
+        """Return True `path` is not in `excluded_paths`
+        with slash tolerance
+        Note: All paths in `excluded_paths` end by a '/'
+        """
+
+        # If None or empty
+        if not path or not excluded_paths:
+            return True
+
+        # Add a '/' to the end of `path` if it doesn't have it
+        if not path.endswith('/'):
+            path += '/'
+
+        # Return the membership test result
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         return None
