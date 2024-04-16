7cdf151f3c93daa97c0f10ca0dd75b8cd7bedcde
Chareeef <youssef.charif.h@gmail.com>
2024-04-16 01:26:52 +0100

    Handle excluded_paths ending with '*'

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,19 +11,31 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Return True `path` is not in `excluded_paths`
         with slash tolerance
-        Note: All paths in `excluded_paths` end by a '/'
+        Note: All paths in `excluded_paths` end by a '/' or '*'
         """
 
         # If None or empty
         if not path or not excluded_paths:
             return True
 
-        # Add a '/' to the end of `path` if it doesn't have it
+        # Add '/' to `path` if missing
         if not path.endswith('/'):
             path += '/'
 
-        # Return the membership test result
-        return path not in excluded_paths
+        for ex_path in excluded_paths:
+
+            # if ex_path ends with '/'
+            if ex_path.endswith('/'):
+                if path == ex_path:
+                    return False
+
+            # if ex_path endswith '*'
+            elif ex_path.endswith('*'):
+                if path[:len(ex_path) - 1] == ex_path[:-1]:
+                    return False
+
+        # Authorization not required
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Return the value of the header request 'Authorization' if it exists
