c61e08266482d25b89b9f1d7a64181fa1bb804bc
Chareeef <youssef.charif.h@gmail.com>
2024-04-22 19:57:53 +0100

    Implement 'POST /users' end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """A Flask application
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 # Create the app
 app = Flask(__name__)
@@ -9,12 +10,33 @@ app = Flask(__name__)
 # Disable strict slashes
 app.url_map.strict_slashes = False
 
+# Instantiate Auth
+AUTH = Auth()
+
 
 @app.route('/')
 def index():
     """Root route
     """
     return jsonify({'message': 'Bienvenue'})
+
+@app.route('/users/', methods=['POST'])
+def users():
+    """End-point to register a user
+    """
+
+    # Retrieve email and password
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # Try to register the user
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({'message': 'email already registered'}), 400
+
+    # The user was successefully registered
+    return jsonify({'email': email, 'message': 'user created'})
 
 
 if __name__ == '__main__':
